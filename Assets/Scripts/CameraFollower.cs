db54da09d4db7c2f0c25566c94a3f81ec39c36f9
anish-dondia <73641288+anish-dondia@users.noreply.github.com>
2022-01-07 20:34:06 +0000

    Intial Commit

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public Transform target;
+
+    public float cameraLockOn = 0.25f;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 cleanPosition = Vector3.Lerp(transform.position, desiredPosition, cameraLockOn);
+        transform.position = cleanPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Animator myAnimator; 
+
+    [SerializeField] float moveSpeed = 3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        myAnimator = GetComponent<Animator>();
+
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+
+        if(xValue != 0 || zValue != 0)
+        {
+            myAnimator.SetTrigger("Run");
+            myAnimator.ResetTrigger("Idle");
+        }
+        else
+        {
+            myAnimator.SetTrigger("Idle");
+            myAnimator.ResetTrigger("Run");
+        }
+        
+    }
+}
